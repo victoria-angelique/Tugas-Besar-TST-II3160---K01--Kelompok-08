dd5ada8b5dcf28f9175933ab6373645c84bd0ede
victoria-angelique <victoriaangelique01@gmail.com>
2023-12-07 16:16:50 +0700

    add database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'sql12.freesqldatabase.com',
+        'username'     => 'sql12668378',
+        'password'     => 'zySlC84I2e',
+        'database'     => 'sql12668378',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
